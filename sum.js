51985997fff0bbc9108719074b9adddea2b840ac
jesus bohorquez <bohorquez866@gmail.com>
2022-01-04 16:59:14 -0400

    adds 3rd task: check if key exists inside obejct and passes the test

--- sum.js
+++ sum.js
@@ -1,7 +1,32 @@
-module.exports = {};
+// module.exports = {};
 
 //? TASK 1
 module.exports.sum = sum = (a = 0, b = 0, c = 0) => a + b + c;
 
 //? TASK 2
 module.exports.sum2 = (...nums) => nums.reduce((acc, val) => acc + val, 0);
+
+//? TASK 3
+
+module.exports.obj = obj = {
+  ap: 1,
+  b: "value",
+  c: 404,
+  watch: { ap: "value", see: { ap: 1, b: 2 } },
+};
+
+module.exports.getPaths = getPaths = (object, key) => {
+  let mymap = new Map(),
+    x = Object.entries(object).reduce(
+      (r, [k, v]) => {
+        if (!v || typeof v !== "object") return r;
+        r.push(...getPaths(v, key).map(([l, v]) => [k + (l && "/") + l, v]));
+        return r;
+      },
+      key in object ? [["", object[key]]] : []
+    );
+
+  mymap.set(key, x);
+
+  return x;
+};
--- sum.test.js
+++ sum.test.js
@@ -11,3 +11,21 @@ test("check if user input params sum properly", () => {
   expect(functions.sum2(1, 2, 4)).toBe(7);
   expect(functions.sum2(1, 2, 3, 4, 5, 6)).toBe(21);
 });
+
+test("checks if a key is inside an object and returns the path in the nested Object", () => {
+  expect(
+    functions.getPaths(
+      {
+        ap: 1,
+        b: "value",
+        c: 404,
+        watch: { ap: "value", see: { ap: 1, b: 2 } },
+      },
+      "ap"
+    )
+  ).toEqual([
+    ["", 1],
+    ["watch", "value"],
+    ["watch/see", 1],
+  ]);
+});
